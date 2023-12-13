b9cda904d15841eb5fde95eb96a45820d3d48deb
theC0mptr0ll3R <moki@m2pi.net>
2023-12-13 15:22:23 +0900

    modified:   shiftSRT.py

--- shiftSRT.py
+++ shiftSRT.py
@@ -5,12 +5,9 @@
 # ^^^ ... takes in "someSRTfile.srt", stretch or modify the SRT timings to match the specified end target ("00:05:42.240")
 # ver. 1.0 : Dec 09, 2023
 
-import codecs, sys, srt, datetime
+import codecs, sys, srt
 
 f = ''
-fList = []
-fdic = {}
-idnum = 1
 original_endTC = ''
 target_endTC = ''
 arg = sys.argv
@@ -33,30 +30,18 @@ else:   # ... convert the target TC to timedelta
 with codecs.open(arg[-1], 'r', encoding='utf_8_sig') as fbuf:
     f = fbuf.read()
 
-fList = f.split('\r\n') # ... just to extract the timestamp data... kinda ugly ;-)
-
-for L in fList: # ... building a dictionary set, linking w/ ID and timecode
-    if ' --> ' in L:
-        L = L.split(' --> ')
-        fdic['<TC_' + str(idnum) + '>'] = L[0]
-        idnum += 1
-        fdic['<TC_' + str(idnum) + '>'] = L[1]
-        idnum += 1
-
-for t in fdic.values(): # ... replacing timecode instance w/ ID for later processing
-    f = f.replace(t, list(fdic.keys())[list(fdic.values()).index(t)])
+sBuf = srt.parse(f)
+sub = list(sBuf)
 
 # ... find out the original SRT timestamp to see how much shrinking/stretching it may require:
-original_endTC = list(fdic.values())[-1]
-original_endTC = srt.srt_timestamp_to_timedelta(original_endTC)
+original_endTC = sub[-1].end
 targetRatio = (target_endTC - shiftTC) / original_endTC
 print('ratio:', round(targetRatio * 100, 2), '%')
 
-for i in fdic.keys():
-    td = srt.srt_timestamp_to_timedelta(fdic[i])
-    td = (td + shiftTC) * targetRatio
-    f = f.replace(i, srt.timedelta_to_srt_timestamp(td))
+for i in range(len(sub)):
+    sub[i].start = (sub[i].start * targetRatio) + shiftTC
+    sub[i].end = (sub[i].end * targetRatio) + shiftTC
 
 with codecs.open(newFileName, 'w', encoding='utf_8_sig') as tf:
-    tf.write(f)
+    tf.write(srt.compose(sub))
 
